506db7f2378401648a3cb907c5fbbf879759d402
Léo Duggan <leo.duggan@alumni.epfl.ch>
2023-03-16 08:34:34 +0100

    Cleaned local build

--- controllers/supervisor/build/release/supervisor.d
+++
@@ -1,8 +0,0 @@
-build/release/supervisor.o: supervisor.c \
- /home/leo/webots/include/controller/c/webots/plugins/robot_window/default.h \
- /home/leo/webots/include/controller/c/webots/plugins/robot_window/robot_wwi.h \
- /home/leo/webots/include/controller/c/webots/robot.h \
- /home/leo/webots/include/controller/c/webots/types.h \
- /home/leo/webots/include/controller/c/webots/nodes.h \
- /home/leo/webots/include/controller/c/webots/supervisor.h \
- /home/leo/webots/include/controller/c/webots/contact_point.h
